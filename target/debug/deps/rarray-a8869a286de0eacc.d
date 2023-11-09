c233b0a3fcbfbcb08ef97db9a05bdbfabd2b6e2f
OfflineBot <Flintstonelg@gmail.com>
2023-11-09 22:49:30 +0100

    gitignore

--- target/debug/deps/rarray-a8869a286de0eacc.d
+++
@@ -1,6 +0,0 @@
-/mnt/c/Users/Flint/Desktop/rarray/lib/target/debug/deps/rarray-a8869a286de0eacc.rmeta: src/lib.rs src/array1/array1.rs
-
-/mnt/c/Users/Flint/Desktop/rarray/lib/target/debug/deps/rarray-a8869a286de0eacc.d: src/lib.rs src/array1/array1.rs
-
-src/lib.rs:
-src/array1/array1.rs:
