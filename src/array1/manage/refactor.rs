e68ab79adb938b51dbc13690b71edc29518a0986
OfflineBot <Flintstonelg@gmail.com>
2023-11-11 12:54:51 +0100

    refactor

---
+++ src/array1/manage/refactor.rs
@@ -0,0 +1,19 @@
+
+use crate::Array1;
+
+impl<T> Array1<T>
+where
+    T: PartialOrd,
+    T: Default + Copy
+{
+    pub fn replace_zero(&mut self, e_minus: T) {
+        let size = self.size;
+
+        for i in 0..size {
+            let value = self.get(i);
+            if value == T::default() {
+                self.set_index(i, e_minus);
+            }
+        }
+    }
+}
---
+++ src/array2/manage/refactor.rs
@@ -0,0 +1,22 @@
+
+use crate::Array2;
+
+impl<T> Array2<T>
+where
+    T: PartialOrd,
+    T: Copy + Default
+{
+    pub fn replace_zero(&mut self, e_minus: T) {
+        let row = self.rows;
+        let col = self.cols;
+
+        for i in 0..row {
+            for j in 0..col {
+                let value = self.get(i, j);
+                if value == T::default() {
+                    self.set(i, j, e_minus);
+                }
+            }
+        }
+    }
+}
