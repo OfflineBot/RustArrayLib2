49054b59bdc5b1068d89b0e9269542e89f6f7d72
OfflineBot <FlintStoneLG@gmail.com>
2023-11-16 21:48:03 +0100

    quick fix

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,3 @@ pub use array1::array1::Array1;
 mod array2;
 pub use array2::array2::Array2;
 
-
-// testing
-#[cfg(test)]
-mod tests {
-    use crate::Array1;
-
-    #[test]
-    fn my_test() {
-        let x: Array1<f32> = Array1::new(3);
-    }
-}
