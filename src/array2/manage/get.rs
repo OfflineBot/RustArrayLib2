59ebfd35ebb71149f30c2d27a7210871412d1286
OfflineBot <FlintStoneLG@gmail.com>
2023-11-16 22:12:29 +0100

    get fix

--- src/array2/manage/get.rs
+++ src/array2/manage/get.rs
@@ -4,7 +4,7 @@ impl<T> Array2<T> {
     #[allow(unused)]
     pub fn get(&self, row: usize, col: usize) -> T {
         if self.rows <= row || self.cols <= col {
-            panic!("cant get the value! sizes dont match: {}x{} > {}x{}", row, col, self.rows, self.cols);
+            panic!("cant get the value! sizes dont match: try_to_get: {}x{} > max_size: {}x{}", row, col, self.rows, self.cols);
         }
         unsafe {
             let ptr = self.array.offset(row as isize * self.cols as isize + col as isize);
