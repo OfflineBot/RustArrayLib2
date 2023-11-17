4c03dd8b123ccc7a7210b08662b8e2d69a144aa1
OfflineBot <flintstonelg@gmail.com>
2023-11-17 11:40:43 +0100

    quick

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,34 @@ pub use array1::array1::Array1;
 mod array2;
 pub use array2::array2::Array2;
 
+
+#[cfg(test)]
+mod tester {
+    use crate::*;
+
+
+    #[test]
+    fn x_y_norm() {
+        let x: Vec<Vec<f32>> = vec![
+            vec![1.0, 1.0],
+            vec![0.0, 0.0],
+        ];
+        let y: Vec<Vec<f32>> = vec![
+            vec![1.0],
+            vec![0.0]
+        ];
+        let input: Array2<f32> = Array2::from_vec(x);
+        let output: Array2<f32> = Array2::from_vec(y);
+
+        let x_mean: Array1<f32> = input.mean();
+        let x_std: Array1<f32> = input.std();
+        let x_norm: Array2<f32> = (input - &x_mean) / &x_std;
+        println!("x_norm: {:?}", x_norm);
+
+        let y_mean: Array1<f32> = output.mean();
+        let y_std: Array1<f32> = output.std();
+        let y_norm: Array2<f32> = (output - &y_mean) / &y_std;
+        println!("y_norm: {:?}", y_norm);
+
+    }
+}
