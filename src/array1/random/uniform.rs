6eacbddfe3690321da8465b3d8ede1868ada8a98
OfflineBot <FlintStoneLG@gmail.com>
2023-11-27 20:43:54 +0100

    random seed

--- src/array1/random/uniform.rs
+++ src/array1/random/uniform.rs
@@ -1,13 +1,13 @@
 use std::ops::AddAssign;
+use rand::SeedableRng;
+use rand::rngs::StdRng;
 use rand::{Rng, thread_rng, distributions::uniform::SampleUniform};
 
 use crate::Array1;
 
-use crate::traits::set_val::SetVal;
-
 impl<T> Array1<T> 
 where
-    T: AddAssign + Clone + SetVal<T> + PartialOrd + SampleUniform + Copy
+    T: AddAssign + Clone + PartialOrd + SampleUniform + Copy
 {
     ///
     /// replaces whole Array1 with random values
@@ -20,8 +20,22 @@ where
 
         let mut rng = thread_rng();
         for i in 0..size {
-            let random_val = rng.gen_range(T::set_val(min)..=T::set_val(max));
+            let random_val = rng.gen_range(min.clone()..=max.clone());
             self.set_index(i, random_val); 
+        }
+    }
+
+    pub fn random_uniform_seed(self, min: T, max: T, seed: u64) {
+        let size = self.size();
+
+        if min > max {
+            panic!("minimum value is bigger than maximum value for random uniform");
+        }
+
+        let mut rng = StdRng::seed_from_u64(seed);
+        for i in 0..size {
+            let random_val = rng.gen_range(min.clone()..=max.clone());
+            self.set_index(i, random_val);
         }
     }
 }
--- src/array2/random/uniform.rs
+++ src/array2/random/uniform.rs
@@ -1,6 +1,6 @@
 
 use std::ops::AddAssign;
-use rand::{Rng, thread_rng, distributions::uniform::SampleUniform};
+use rand::{Rng, thread_rng, distributions::uniform::SampleUniform, rngs::StdRng, SeedableRng};
 
 use crate::Array2;
 
@@ -10,7 +10,26 @@ where
 {
     pub fn random_uniform(&self, min: T, max: T) {
         
+        if min > max {
+            panic!("minimum value is bigger than maximum value for random uniform");
+        }
+
         let mut rng = thread_rng();
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                let random_val = rng.gen_range(min.clone()..=max.clone());
+                self.set(i, j, random_val);
+            }
+        }
+    }
+
+    pub fn random_uniform_seed(self, min: T, max: T, seed: u64) {
+        
+        if min > max {
+            panic!("minimum value is bigger than maximum value for random uniform");
+        }
+
+        let mut rng = StdRng::seed_from_u64(seed);
         for i in 0..self.rows {
             for j in 0..self.cols {
                 let random_val = rng.gen_range(min.clone()..=max.clone());
